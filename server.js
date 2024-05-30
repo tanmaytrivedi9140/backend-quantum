81a8b6345e5c72af916e935fa822504dd3295800
Tanmay Trivedi <tanmaytrivedi57@gmail.com>
2024-05-30 14:05:01 +0530

    removal of cors

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-
-const cors = require("cors");
 require("dotenv").config();
  const connectDb = require("./dbconfig/dbconfig");
 
